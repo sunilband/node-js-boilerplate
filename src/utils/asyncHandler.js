ebe08aaba1b473a2d95a761b0107b6aa472810a2
Sunil Band <111903120+sunilband@users.noreply.github.com>
2024-01-05 11:11:30 +0530

    Update asyncHandler.js
    incorrect syntax : err replaced with error

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -3,7 +3,7 @@ const asyncHandler = (fn) => async(req, res, next) => {
     try {
         await fn(req, res, next);
     } catch (error) {
-        res.status(err.code || 500).json({
+        res.status(error.code || 500).json({
             success: false,
             message: error.message || "Internal Server Error",
         });
